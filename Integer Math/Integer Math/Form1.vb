a4c8dfc878268a3184397f7788f4598f64e3124d
shamusi <Shamusi@op.pl>
2018-11-06 21:31:58 +0100

    first step

---
+++ Integer Math/Integer Math/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub btnIntMath_Click(sender As Object, e As EventArgs) Handles btnIntMath.Click
+        'Set number , divide numbers, and display results
+        Dim intNumber = 12
+        intNumber += 6
+        MessageBox.Show("Division test." & intNumber.ToString, "Interger Math")
+    End Sub
+End Class
