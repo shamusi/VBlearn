7c31949c0e7d162b92830acf864b795f490b2e7e
shamusi <Shamusi@op.pl>
2019-03-19 23:57:25 +0100

    created tryVBNet project

---
+++ TryVBNET/TestingVB/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim mytest As String = "My name is Shamusi"
+
+        Dim CA() As Char = mytest.ToCharArray
+        Array.Reverse(CA)
+
+        For i = 0 To CA.Length - 1
+            Console.WriteLine(CA(i))
+        Next
+
+        Console.Read()
+    End Sub
+
+End Module
