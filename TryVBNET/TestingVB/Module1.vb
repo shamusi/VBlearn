4efa0d0aa25c83b2da00d23d095ccc4c9622113c
shamusi <Shamusi@op.pl>
2019-03-23 12:20:01 +0100

    Done2

--- TryVBNET/TestingVB/Module1.vb
+++ TryVBNET/TestingVB/Module1.vb
@@ -2,45 +2,6 @@
 
     Sub Main()
 
-        Console.WriteLine("The Name Game")
-
-        Console.Write("what is your name")
-        Dim firstname As String = Console.ReadLine()
-
-        Console.Write("what is your last name")
-        Dim secondName As String = Console.ReadLine()
-
-
-        Dim charfirstname As Char()
-        charfirstname = firstname.ToCharArray
-        Array.Reverse(charfirstname)
-
-        Dim charlastname As Char()
-        charlastname = secondName.ToCharArray
-        Array.Reverse(charlastname)
-
-        'Console.WriteLine(charfirstname)
-        'Console.WriteLine(charlastname)
-
-        For Each items In charfirstname
-            Console.WriteLine(items)
-
-        Next
-
-
-
-
-        For index = 0 To charlastname.Length - 1
-
-            Console.WriteLine(charlastname(index))
-
-        Next
-
-        Console.ReadLine()
-
-
-
-
     End Sub
 
 End Module
