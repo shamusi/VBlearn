aedfeb1a75b0edfb609b565d969fdc98ae814d81
shamusi <Shamusi@op.pl>
2019-03-24 20:24:32 +0100

    done5

---
+++ TryVBNET/TestingVB/CAR.vb
@@ -0,0 +1,43 @@
+﻿Public Class CAR
+
+    Public Property make As String
+    Public Property Model As String
+    Public Property Year As Integer
+    Public Property color As String
+
+    Public Function makevalue(year As Integer) As Decimal
+
+        Dim carvalue As Decimal
+        If year > 1990 Then
+            carvalue = 1000.0
+        Else
+            carvalue = 20000.0
+        End If
+        Return carvalue
+    End Function
+
+
+    Public Sub New()
+        make = "NiSSAN"
+    End Sub
+
+    Public Sub New(ByVal makevalue As String,
+                   ByVal modelvalue As String,
+                   ByVal yearvalue As Integer,
+                   ByVal colorvalue As String)
+
+        make = makevalue
+        Model = modelvalue
+        Year = yearvalue
+        color = colorvalue
+    End Sub
+
+
+
+End Class
+Public Class carlot
+    Public Shared Sub Addtoinv(ByVal newcar As CAR)
+
+
+    End Sub
+End Class
--- TryVBNET/TestingVB/Module1.vb
+++ TryVBNET/TestingVB/Module1.vb
@@ -1,7 +1,33 @@
-﻿Module Module1
+﻿
+Module Module1
 
     Sub Main()
 
+        'Dim MyCar As New CAR()
+
+        'Dim mymake As String
+        'mymake = Console.ReadLine()
+        'MyCar.make = "KIA"
+
+        'MyCar.Model = "SUV"
+        'MyCar.color = "Blue"
+        'MyCar.Year = 1920
+
+        Dim myCar As New CAR("Hyndia ", "4 runner", 2010, "white")
+        carlot.Addtoinv(myCar)
+
+        Console.WriteLine("{0} {1} {2}", myCar.make.ToString, dMv("ll"), myCar.color)
+        Console.WriteLine("{0} {1} {2}", MyCar.make.ToString, dMv("ll"), MyCar.makevalue(2000))
+        'Console.WriteLine(MyCar.Year)
+        Console.ReadLine()
     End Sub
+
+    Function dMv(ByVal mycarw As Char) As Decimal
+
+        Return 100.0
+    End Function
+
+
+
 
 End Module
